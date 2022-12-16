fb4767219347ca4277d0fcf58eb1b094eba68f62
9wilson6 <gatheruwilson@gmail.com>
2022-12-16 12:32:16 +0300

    greet the world using a function

---
+++ Hello World/helloWorld2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * use a function to greet the world
+*/
+
+int main()
+{
+    //call the greetings function
+    greetings();
+
+    return (0);
+}
+
+/*void function to greet the world*/
+
+void greetings()
+{
+    //print greetings
+    printf("hello world");
+}
